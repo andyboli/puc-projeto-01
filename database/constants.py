4e6b44ab5a58375530afb297aed1e42f0df2f5bd
Anderson <webdevelopmentbolivar@gmail.com>
2022-10-16 15:41:31 -0300

    unique

--- database/constants.py
+++ database/constants.py
@@ -14,3 +14,33 @@ DB_NAME = 'puc_database'
 CREATE_DATABASE_QUERY = "CREATE DATABASE IF NOT EXISTS {} DEFAULT CHARACTER SET 'UTF8MB4'"
 
 DROP_DATABASE_QUERY = "DROP DATABASE IF EXISTS {}"
+
+TABLES = {}
+
+TABLES['homeless'] = {}
+
+TABLES['homeless']["create"] = (
+    '''CREATE TABLE IF NOT EXISTS `{}`.`homeless` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `year` INT NOT NULL,
+    `age` INT NULL,
+    `bolsa_familia` TINYINT NOT NULL,
+    `region` VARCHAR(45) NULL,
+    `period` VARCHAR(45) NOT NULL,
+    `birthday` VARCHAR(45) NULL,
+    `gender` VARCHAR(45) NULL,
+    `ethnicity` VARCHAR(45) NULL,
+    `schooling` VARCHAR(45) NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `idhomeless_UNIQUE` (`id` ASC) VISIBLE)
+    ENGINE = InnoDB''')
+
+
+TABLES['homeless']["insert"] = (
+    '''INSERT INTO `{0}`.`homeless`
+    (period, birthday, age, gender, bolsa_familia, schooling, ethnicity, region, year)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)''')
+
+
+TABLES['homeless']['header'] = ['period', 'birthday', 'age', 'gender',
+                                'bolsa_familia', 'schooling', 'ethnicity', 'region', 'year']
--- database/queries.py
+++ database/queries.py
@@ -19,3 +19,43 @@ def create_database(connection: MySQLConnection):
         print(reader.lang('database_create_database_done').format(constants.DB_NAME))
     finally:
         cursor.close()
+
+
+def create_tables(connection: MySQLConnection):
+    try:
+        print(reader.lang('database_create_tables_start'))
+        cursor = connection.cursor()
+        tables: dict[str, str] = constants.TABLES
+        for table in tables:
+            try:
+                CREATE_TABLE_QUERY = tables[table]["create"]
+                cursor.execute(CREATE_TABLE_QUERY.format(
+                    constants.DB_NAME))
+            except mysql.connector.Error as err:
+                if err.errno != errorcode.ER_TABLE_EXISTS_ERROR:
+                    print(reader.lang(
+                        'database_create_tables_table_error').format(table, err))
+                    exit(1)
+    except mysql.connector.Error as err:
+        print(reader.lang('database_create_tables_error').format(err))
+    else:
+        print(reader.lang('database_create_tables_done'))
+    finally:
+        cursor.close()
+
+
+def insert_table(connection: MySQLConnection, table: str, data: list):
+    try:
+        print(reader.lang('database_insert_table_start').format(table))
+        cursor = connection.cursor()
+        INSERT_DATABASE_QUERY: str = constants.TABLES[table]["insert"].format(
+            constants.DB_NAME)
+        cursor.executemany(INSERT_DATABASE_QUERY, data)
+        connection.commit()
+    except mysql.connector.Error as err:
+        print(reader.lang('database_insert_table_error').format(table, err))
+    else:
+        print(reader.lang('database_insert_table_done').format(
+            table, cursor.rowcount))
+    finally:
+        cursor.close()
