eb56772cbee1459ed90108fd6e9253c71cf19f84
Anderson <webdevelopmentbolivar@gmail.com>
2022-11-14 19:34:48 -0300

    add matplotlib

--- view/app.py
+++ view/app.py
@@ -1,7 +1,14 @@
 from controller import (reader)
 from database import (connection)
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def app():
     print(reader.lang('main_title'))
     connection.run()
+    # Data
+    xpoints = np.array([0, 6])
+    ypoints = np.array([0, 250])
+    plt.plot(xpoints, ypoints)
+    plt.savefig("mygraph.png")
