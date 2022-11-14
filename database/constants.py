f402d8f124cbc0e29204589dc7c811e9d2423b16
Anderson <webdevelopmentbolivar@gmail.com>
2022-11-14 19:09:11 -0300

    fix homeless table

--- database/constants.py
+++ database/constants.py
@@ -9,6 +9,7 @@ CONFIG = {
     'raise_on_warnings': True
 }
 
+
 DB_NAME = 'puc_database'
 
 CREATE_DATABASE_QUERY = "CREATE DATABASE IF NOT EXISTS {} DEFAULT CHARACTER SET 'UTF8MB4'"
@@ -22,15 +23,15 @@ TABLES['homeless'] = {}
 TABLES['homeless']["create"] = (
     '''CREATE TABLE IF NOT EXISTS `{}`.`homeless` (
     `id` INT NOT NULL AUTO_INCREMENT,
-    `year` INT NOT NULL,
+    `month_year` VARCHAR(45) NOT NULL,
     `age` INT NULL,
-    `bolsa_familia` TINYINT NOT NULL,
+    `gender` VARCHAR(45) NULL,
+    `birthday` VARCHAR(45) NULL,
+    `schooling` VARCHAR(45) NULL,
+    `ethnicity` VARCHAR(45) NULL,
     `region` VARCHAR(45) NULL,
     `period` VARCHAR(45) NOT NULL,
-    `birthday` VARCHAR(45) NULL,
-    `gender` VARCHAR(45) NULL,
-    `ethnicity` VARCHAR(45) NULL,
-    `schooling` VARCHAR(45) NULL,
+    `social_welfare` TINYINT NULL,
     PRIMARY KEY (`id`),
     UNIQUE INDEX `idhomeless_UNIQUE` (`id` ASC) VISIBLE)
     ENGINE = InnoDB''')
@@ -38,9 +39,9 @@ TABLES['homeless']["create"] = (
 
 TABLES['homeless']["insert"] = (
     '''INSERT INTO `{0}`.`homeless`
-    (period, birthday, age, gender, bolsa_familia, schooling, ethnicity, region, year)
+    (month_year, age, gender, birthday, schooling, ethnicity, region, period, social_welfare)
     VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)''')
 
 
-TABLES['homeless']['header'] = ['period', 'birthday', 'age', 'gender',
-                                'bolsa_familia', 'schooling', 'ethnicity', 'region', 'year']
+TABLES['homeless']['header'] = ['month_year', 'age', 'gender', 'birthday',
+                                'schooling', 'ethnicity', 'region', 'period', 'social_welfare']
