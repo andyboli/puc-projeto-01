ef85d67986c94789cdbad652a8d78643521141a6
andyboli <webdevelopmentbolivar@gmail.com>
2022-12-12 20:05:06 -0300

    fix connection

--- index.py
+++ index.py
@@ -1,17 +1,19 @@
 from view.app import run_view
-# from controller.orchestrator import restart_app
+from controller.orchestrator import restart_app
 
 
 if __name__ == "__main__":
-    # try:
-    run_view()
-    # finally:
-    #     while True:
-    #         try:
-    #             message, error = next(restart_app)
-    #             if message:
-    #                 print(message)
-    #             if error:
-    #                 print(error)
-    #         except StopIteration:
-    #             break
+    try:
+        run_view()
+    finally:
+        while True:
+            try:
+                success, loading, error = next(restart_app)
+                if loading:
+                    print(loading)
+                elif success:
+                    print(success)
+                elif error:
+                    print(error)
+            except StopIteration:
+                break
